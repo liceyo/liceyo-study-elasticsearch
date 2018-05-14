fecc88bbf65e37e6ea553f6350079eecb1a3cd5c
liceyo <1396599326@qq.com>
2018-05-14 16:13:07 +0800

    初始化项目

---
+++ src/main/java/com/liceyo/elasticsearch/LiceyoStudyElasticsearchApplication.java
@@ -0,0 +1,17 @@
+package com.liceyo.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author lichy
+ * @version 2018/5/14
+ */
+@SpringBootApplication
+public class LiceyoStudyElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiceyoStudyElasticsearchApplication.class, args);
+	}
+}
